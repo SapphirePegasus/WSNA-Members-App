cc2cebdc70d6969cb6c22dd1b45b1e2b56a7d7c7
Prittam Bhattacharyya <59970935+SapphirePegasus@users.noreply.github.com>
2026-04-12 22:56:37 +0530

    Delete src directory

--- src/app/(pages)/union/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Union() {
-  return (
-    <div>union</div>
-  );
-}
--- src/app/components/global/MsalProviderClient.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/MsalProviderClient.tsx
-"use client";
-import React from "react";
-import { MsalProvider } from "@azure/msal-react";
-import { msalInstance } from "@/app/lib/msalConfig";
-
-export default function MsalProviderClient({ children }: { children: React.ReactNode }) {
-  return <MsalProvider instance={msalInstance}>{children}</MsalProvider>;
-}
