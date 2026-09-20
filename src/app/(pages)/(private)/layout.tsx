667efd14372ad41e10813a784ed7445befde10e5
Prittam Bhattacharyya <59970935+SapphirePegasus@users.noreply.github.com>
2026-09-20 16:13:31 +0530

    Delete src/app directory

--- src/app/(pages)/(private)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import PrivateShell from "@/app/components/global/PrivateShell";
-
-export default function PrivateLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <PrivateShell>{children}</PrivateShell>;
-}
